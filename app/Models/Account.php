533b06d26d6e566fa34221c06896277cd5225b3f
Andrey Legkiy <legkiy.info@mail.ru>
2022-03-05 02:50:37 +0300

    Создание отношений

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    public function operations()
+    {
+        return $this->hasMany(Operation::class);
+    }
 }
