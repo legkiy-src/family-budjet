5060599dcd60b9a70bf08d1f927ab474c8070887
legkiy-src <legkiy.andrey@mail.ru>
2022-09-26 00:21:36 +0300

    Вывод таблицы счетов

--- resources/views/accounts/accounts.blade.php
+++ resources/views/accounts/accounts.blade.php
@@ -1,1 +1,83 @@
 @extends('layouts.app')
+@section('content')
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 bg-white overflow-hidden shadow-sm sm:rounded-lg"  style="min-width: 1232px;">
+        <table id="data" class="table table-bordered table-hover">
+            <thead>
+            <tr class="text-center">
+                <th>Название счёта</th>
+                <th>Остаток</th>
+                <th>Валюта</th>
+                <th>Описание</th>
+            </tr>
+            </thead>
+            <tbody>
+            @forelse($accounts as $account)
+                <tr>
+                    <td>{{ $account['name'] }}</td>
+                    <td>{{ number_format($account['balance'] / 100, 2) }}</td>
+                    <td>{{ $account['currency'] }}</td>
+                    <td>{{ $account['description'] }}</td>
+                </tr>
+            @empty
+                <div class="text-danger m-2">Список пуст</div>
+            @endforelse
+            </tbody>
+            <tfoot>
+            </tfoot>
+        </table>
+    </div>
+@endsection
+@push('styles')
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
+
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
+
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
+
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
+    </style>
+@endpush
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,50 +3,32 @@ use Illuminate\Support\Facades\Route;
 
 $route = Route::currentRouteName();
 ?>
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <nav class="navbar navbar-expand-lg bg-light">
-                        <div class="container-fluid">
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarNav">
-                                <ul class="navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link {{ $route === 'accounts' ? 'active' : '' }}" aria-current="page" href="{{ route('accounts') }}">Счета</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link {{ $route === 'operations' ? 'active' : '' }}" href="{{ route('operations') }}">Операции</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link {{ $route === 'articles' ? 'active' : '' }}" href="{{ route('articles') }}">Статьи</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link {{ $route === 'currencies' ? 'active' : '' }}" href="{{ route('currencies') }}">Валюты</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link {{ $route === 'reports' ? 'active' : '' }}" href="{{ route('reports') }}">Отчёты</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('accounts') }}">{{ config('app.name') }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
 
-            <!-- Settings Dropdown -->
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link {{ $route === 'accounts' ? 'active' : '' }}" aria-current="page" href="{{ route('accounts') }}">Счета</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $route === 'operations' ? 'active' : '' }}" href="{{ route('operations') }}">Операции</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $route === 'articles' ? 'active' : '' }}" href="{{ route('articles') }}">Статьи</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $route === 'currencies' ? 'active' : '' }}" href="{{ route('currencies') }}">Валюты</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $route === 'reports' ? 'active' : '' }}" href="{{ route('reports') }}">Отчёты</a>
+                </li>
+            </ul>
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -67,53 +49,13 @@ $route = Route::currentRouteName();
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
     </div>
